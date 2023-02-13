c982a2cc06bd58cfb4eaed23ad2a92023e7e8273
Qwerty <qwerty@mail.ru>
2023-02-13 16:38:47 +0300

    Показать вторую цифру трёхзначного числа

---
+++ Hoome7/Program.cs
@@ -0,0 +1,10 @@
+﻿//Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int c;
+while (number >= 100)
+{
+    number = number / 10;
+}
+c = number % 10;
+Console.WriteLine("Второе число: " + c);
