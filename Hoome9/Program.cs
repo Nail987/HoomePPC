beb81929d06c649affce671c8a6c57c56e1b6270
Qwerty <qwerty@mail.ru>
2023-02-13 17:08:06 +0300

    Удалить вторую цифру трёхзначного числа

---
+++ Hoome9/Program.cs
@@ -0,0 +1,8 @@
+﻿//Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int oneDigit = number / 100;
+int twoDigit = (number / 100 * oneDigit) / 10;
+int thirdDigit = number - 100 * oneDigit - 10 * twoDigit;
+int newNumber = oneDigit * 10 + thirdDigit;
+Console.WriteLine(newNumber);
