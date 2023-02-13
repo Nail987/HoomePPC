08f0515b10dccaa578b6f1319cc4c1f20981df27
Qwerty <qwerty@mail.ru>
2023-02-13 14:11:55 +0300

    1 задание

---
+++ Hoome1/Program.cs
@@ -0,0 +1,19 @@
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+Console.WriteLine("Enter first number: ");
+	int a = int.Parse(Console.ReadLine()!);
+	Console.WriteLine("Enter second number: ");
+	int b = int.Parse(Console.ReadLine()!);
+	int sum=a*a;
+	if (sum==b)
+	{
+    	Console.WriteLine("Является:"+sum);
+    
+	} 
+	else if (sum>b){
+
+    	Console.WriteLine("Не является квадратом:"+sum);
+	} 
+	else if (sum<b) {
+
+    	Console.WriteLine("Не является квадратом:"+sum);
+	} 
