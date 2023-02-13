a1277a106b5135b6c3bd29193b69e982d0a0a8e3
Qwerty <qwerty@mail.ru>
2023-02-13 16:16:21 +0300

    Выяснить является ли число чётным

---
+++ Hoome4/Program.cs
@@ -0,0 +1,10 @@
+﻿// //Выяснить является ли число чётным
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+a=a%2;
+if (a==0)
+{
+    Console.WriteLine("Ваше число четное: "+a);
+} 
+else 
+Console.WriteLine("Ваше число нечетное: "+a);
