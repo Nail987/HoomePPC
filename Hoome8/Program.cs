c7fb2bc2e1cc53171243c81d8e8ac37d206cf56d
Qwerty <qwerty@mail.ru>
2023-02-13 16:46:47 +0300

    Дано число из отрезка [10, 99]. Показать наибольшую цифру числ

---
+++ Hoome8/Program.cs
@@ -0,0 +1,11 @@
+﻿//8 Дано число из отрезка [10, 99]. Показать наибольшую цифру числ
+int m = 10;
+int n = 99;
+int max = 0;
+int[] array = new int[n - m + 1];
+for (int max[i] = 0; i < array.Length; i++)
+{
+    array[i] = m++;
+
+}
+Console.WriteLine(String.Join(",", array));
