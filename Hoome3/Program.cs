e499c3599c64af1b50a8c18746a4f815095fcec2
Qwerty <qwerty@mail.ru>
2023-02-13 16:10:25 +0300

    3 задание

---
+++ Hoome3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Найти максимальное из трех чисел
+Console.WriteLine("Введите первое число: ");
+int numA =int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int numB = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int numC =int.Parse(Console.ReadLine()!);
+int max = numA;
+if (numB>max) max=numB;
+if(numC>max) max=numC;
+{
+    Console.WriteLine(max);
+}
