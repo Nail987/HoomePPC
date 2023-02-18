00c637a19aa874e81e06b943267a63ec27521fa9
nail <siam93@yandedx.ru>
2023-02-18 13:42:43 +0300

    Подсчитать сумму цифр в числе

---
+++ Ham4/Program.cs
@@ -0,0 +1,18 @@
+﻿//3.3 Подсчитать сумму цифр в числе
+
+Console.Clear();
+Console.WriteLine();
+
+int number = new Random().Next(1, 100000);
+int sumDigit = 0;
+Console.Write($"Сумма цифр числа {number} равна ");
+
+while (number > 0)
+{
+    int digit = number % 10;
+    sumDigit = sumDigit + digit;
+    number = number / 10;
+}
+
+Console.WriteLine(sumDigit);
+Console.WriteLine();
